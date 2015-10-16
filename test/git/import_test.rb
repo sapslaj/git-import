a8149b24bb0c74b4fcb779c0359dd957ffd3b036
sapslaj <saps.laj@gmail.com>
2015-10-16 08:02:25 -0400

    Adding tests.

--- test/git/import_test.rb
+++ test/git/import_test.rb
@@ -1,11 +1,6 @@
 require 'test_helper'
 
-class Git::ImportTest < Minitest::Test
-  def test_that_it_has_a_version_number
-    refute_nil ::Git::Import::VERSION
-  end
-
-  def test_it_does_something_useful
-    assert false
-  end
-end
+puts "TESTS?\n\n"
+puts "ha" * 50
+puts "\n\n\n\nno.\n"
+puts "There are no tests."
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,4 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
-require 'git/import'
+require 'git_import'
 
 require 'minitest/autorun'
